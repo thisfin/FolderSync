029c286ceff4ba98525cf4ff1ec772fecc78e2cd
文优 <wenyou.ly@taobao.com>
2017-09-08 17:07:02 +0800

    update

--- FolderSync/FileService.swift
+++ FolderSync/FileService.swift
@@ -9,9 +9,32 @@
 import Cocoa
 
 class FileService {
+    func traversalFolder(sourceFile: FileObject, targetFile: FileObject) -> (FileObject, FileObject) {
+        let source = FileObject.init(path: sourceFile.path)
+        let target = FileObject.init(path: targetFile.path)
 
-    func traversalFolder(path: String) {
+        var sourceIterator = sourceFile.subFiles.makeIterator()
+        var targetIterator = targetFile.subFiles.makeIterator()
 
+        while true {
+            let s = sourceIterator.next()
+            let t = targetIterator.next()
+
+            if s != nil && t != nil {
+                if s!.name > t!.name {
+
+                } else {
+
+                }
+            } else if s != nil {
+
+            } else if t != nil {
+
+            } else {
+
+            }
+        }
+
+        return (source, target)
     }
-
 }
