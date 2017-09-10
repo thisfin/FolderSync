45eeee96016d4296a01518089f364433cad8d127
文优 <thisfin@gmail.com>
2017-09-10 23:52:18 +0800

    update

--- FolderSync/FileService.swift
+++ FolderSync/FileService.swift
@@ -27,14 +27,87 @@ class FileService {
 
                 }
             } else if s != nil {
-
+                
             } else if t != nil {
 
             } else {
-
+                break
             }
         }
 
         return (source, target)
+    }
+
+    func compare(sourceFile: FileObject!, targetFile: FileObject!) {
+        if sourceFile == nil && targetFile == nil {
+            // over
+        } else if sourceFile == nil {
+            // 处理 target
+            handleTarget()
+            // source 是否补白
+        } else if targetFile == nil {
+            // 处理 sourece
+            handleSource()
+            // target 是否补白
+        } else {
+            // 处理
+            compare1(sourceFile: sourceFile, targetFile: targetFile)
+        }
+    }
+
+    func compare1(sourceFile: FileObject, targetFile: FileObject) {
+        if sourceFile.name == targetFile.name {
+            handleDouble(sourceFile: sourceFile, targetFile: targetFile)
+        } else if sourceFile.name > targetFile.name {
+            // 处理 target, target next
+            handleTarget()
+            // source 是否补白
+
+            // target next
+        } else {
+            // 处理 source, source next
+            handleSource()
+            // target 是否补白
+
+            // source next
+        }
+    }
+
+    func handleDouble(sourceFile: FileObject, targetFile: FileObject) {
+        if sourceFile.type != targetFile.type {
+            if sourceFile.type == .folder {
+                // source 递归
+                // target file
+            } else {
+                // source file
+                // target 递归
+            }
+        } else {
+            if sourceFile.type == .folder {
+                // 双递归
+            } else {
+                // 文件 size & time 比较
+                let cr = sourceFile.modificationDate!.compare(targetFile.modificationDate! as Date)
+                switch cr {
+                case .orderedSame:
+                    // 相同
+                    ()
+                case .orderedAscending:
+                    // source 旧
+                    ()
+                case .orderedDescending:
+                    // target 旧
+                    ()
+                }
+            }
+        }
+    }
+
+    func handleSource() {
+        // 递归设置only
+    }
+
+    func handleTarget() {
+        // 递归设置only
     }
 }
